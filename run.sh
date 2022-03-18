82d075f9409ba43e161403ed0ef890a8d687bc1a
Christian Lowe <christianlowe.cl@gmail.com>
2022-03-18 13:34:32 -0700

    Resolve issue running in local Docker container

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
-mvn clean compile assembly:single && docker build -t discordle . && docker run -d --env DISCORDLE_TOKEN discordle:latest
+mvn clean package spring-boot:repackage \
+  && docker build -t discordle . \
+  && docker run -d --env DISCORDLE_TOKEN discordle:latest
